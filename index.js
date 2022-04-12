906efd4d3ad70a5f7719d03ed9b7c0c0e534a193
xu-ashley <ashley.xu321@gmail.com>
2022-04-11 23:34:01 -0400

    basic styling & addMark()

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("Hello");
+
+// $("#0-0").text("X");
+
+function addMark(row, column, mark) {
+  $("#" + row + "-" + column).text(mark);
+}
+
+addMark(1, 2, "O");
+
+// [["X", "", "O"], ["", "", "O"],["X", "", ""]]
