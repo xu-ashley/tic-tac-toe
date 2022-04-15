4cfc61fc992d22a5fdb05d9c80a3e62cedc0ab6d
xu-ashley <ashley.xu321@gmail.com>
2022-04-15 01:14:54 -0400

    center game board, add pseudocode file

--- index.js
+++ index.js
@@ -9,3 +9,52 @@ function addMark(row, column, mark) {
 addMark(1, 2, "O");
 
 // [["X", "", "O"], ["", "", "O"],["X", "", ""]]
+
+function isWin(
+	grid, /* [["X", ...], ...] */
+	player /* "X" or "O" */
+) {
+	for (let r = 0; r < 3; r++) {
+		if (isHorizontalWin(grid, r, player)) {
+			return true;
+		}
+	}
+
+	// for (let c ...) {
+	//	 isVerticalWin(...);
+	// }
+
+	if (isDiagonalUpWin(grid, player)) {
+		return true;
+	}
+
+	// if (isDiagonalDownWin(...) { ... }
+
+	return false;
+}
+
+isHorizontalWin(grid, "X", 0);
+
+function isHorizontalWin(grid, player, r) {
+	for (let c = 0; c < 3; c++) {
+    if (grid[r][c] !== player) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// . . X
+// . X .
+// X . .
+function isDiagonalUpWin(grid, player) {
+	return (
+		grid[2][0] === player &&
+		grid[1][1] === player &&
+		grid[0][2] === player
+	);
+}
+
+// X . .
+// . X .
+// . . X
---
+++ pseudocode.md
@@ -0,0 +1,29 @@
+#Titles
+##Title 2
+**bold**
+[Links](link.com)
+1. asdf
+2. asdf
+
+#Wins
+- [starting code from pandu!](https://pastebin.com/CVT2cVkN)
+
+Basic
+1. Player X
+2. Player O
+
+TO-DO:
+ [x]center game board
+
+Clicks
+- click a box, put a **mark**
+  - push "mark" to array
+  - console.log arrays
+
+
+
+
+
+
+Extras Pretty
+- Invert colors button!
