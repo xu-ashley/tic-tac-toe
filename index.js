9c033c44b0f7b63b07aa3099b5bda88230a1dd2a
xu-ashley <ashley.xu321@gmail.com>
2022-04-15 13:50:28 -0400

    toggle "X" and "O" marks

--- index.js
+++ index.js
@@ -1,60 +1,85 @@
 console.log("Hello");
 
+var player = "O";
+
 // $("#0-0").text("X");
+
+
+
 
 function addMark(row, column, mark) {
   $("#" + row + "-" + column).text(mark);
+
+  if (player == "X") {
+    player = "O";
+  } else {
+    player = "X";
+  };
 }
 
 addMark(1, 2, "O");
 
 // [["X", "", "O"], ["", "", "O"],["X", "", ""]]
 
-function isWin(
-	grid, /* [["X", ...], ...] */
-	player /* "X" or "O" */
-) {
-	for (let r = 0; r < 3; r++) {
-		if (isHorizontalWin(grid, r, player)) {
-			return true;
-		}
-	}
 
-	// for (let c ...) {
-	//	 isVerticalWin(...);
-	// }
-
-	if (isDiagonalUpWin(grid, player)) {
-		return true;
-	}
-
-	// if (isDiagonalDownWin(...) { ... }
-
-	return false;
-}
-
-isHorizontalWin(grid, "X", 0);
-
-function isHorizontalWin(grid, player, r) {
-	for (let c = 0; c < 3; c++) {
-    if (grid[r][c] !== player) {
-			return false;
-		}
-	}
-	return true;
-}
-
-// . . X
-// . X .
-// X . .
-function isDiagonalUpWin(grid, player) {
-	return (
-		grid[2][0] === player &&
-		grid[1][1] === player &&
-		grid[0][2] === player
-	);
-}
-
-// X . .
-// . X .
-// . . X
+//
+//
+// var grid = [
+//   [0-0, 0-1, 0-2],
+//   [1-0, 1-1, 1-2],
+//   [2-0, 2-1, 2-2]
+// ];
+//
+// function addStrike(){
+//
+// }
+//
+//
+// function isWin(
+// 	grid, /* [["X", ...], ...] */
+// 	player /* "X" or "O" */
+// ) {
+// 	for (let r = 0; r < 3; r++) {
+// 		if (isHorizontalWin(grid, r, player)) {
+// 			return true;
+// 		}
+// 	}
+//
+// 	// for (let c ...) {
+// 	//	 isVerticalWin(...);
+// 	// }
+//
+// 	if (isDiagonalUpWin(grid, player)) {
+// 		return true;
+// 	}
+//
+// 	// if (isDiagonalDownWin(...) { ... }
+//
+// 	return false;
+// }
+//
+// isHorizontalWin(grid, "X", 0);
+//
+// function isHorizontalWin(grid, player, r) {
+// 	for (let c = 0; c < 3; c++) {
+//     if (grid[r][c] !== player) {
+// 			return false;
+// 		}
+// 	}
+// 	return true;
+// }
+//
+// // . . X
+// // . X .
+// // X . .
+// function isDiagonalUpWin(grid, player) {
+// 	return (
+// 		grid[2][0] === player &&
+// 		grid[1][1] === player &&
+// 		grid[0][2] === player
+// 	);
+// }
+//
+// // X . .
+// // . X .
+// // . . X
--- pseudocode.md
+++ pseudocode.md
@@ -8,11 +8,11 @@
 #Wins
 - [starting code from pandu!](https://pastebin.com/CVT2cVkN)
 
-Basic
+## Basic
 1. Player X
 2. Player O
 
-TO-DO:
+##T O-DO:
  [x]center game board
 
 Clicks
@@ -25,5 +25,62 @@ Clicks
 
 
 
-Extras Pretty
+## Extras Pretty
 - Invert colors button!
+
+
+## HTML
+Player X's turn
+Player O's turn
+
+Player _ wins!
+Tie!
+
+- if click on a space w/ text in it: "PLEASE CLICK EMPTY SQUARE"
+
+## Wins Attempt
+array row 0
+Array row 1:
+array row 2:
+
+var grid = [];
+
+var grid = [
+  [0-0, 0-1, 0-2],
+  [1-0, 1-1, 1-2],
+  [2-0, 2-1, 2-2]
+];
+
+
+grid[0] = [0-0, 0-1, 0-2];
+grid[1] = [1-0, 1-1, 1-2];
+grid[2] = [2-0, 2-1, 2-2];
+
+- when cell clicked, push "player" value to that position; event listener (onClick)
+  - onClick class = "cell",
+  push player value (X or O) to grid.indexOf[id]
+
+onClick:
+  - push grid.splice(grid.indexOf(#id), 1, player);
+  - addMark
+
+array.splice(array.indexOf(valueToReplace), 1, newValue);
+
+
+ok so player X goes...then "player O's turn!" and player O goes..
+- if player = "X", change to player = O
+
+if (value == 'string0') {
+	value = 'string1';
+} else {
+	value = 'string0';
+}
+
+player = "X"
+
+
+if (player == "X") {
+	player = "O";
+} else {
+	player = "X";
+}
